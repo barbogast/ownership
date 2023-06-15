e90ed6de8db01ce6d9fac80b35e37a80d84f2d1e
Benjamin Arbogast <benjamin.arbogast@gmail.com>
2023-06-15 15:18:02 +0200

    Add warning message for pie charts

--- src/display/PieChartDisplay.tsx
+++ src/display/PieChartDisplay.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Cell, Pie, PieChart } from "recharts";
+import { Alert } from "antd";
 import { QueryExecResult } from "../Db";
 import { COLORS } from "../constants";
 
@@ -7,51 +8,61 @@ type Props = { queryResult: QueryExecResult };
 const PieChartDisplay: React.FC<Props> = ({ queryResult }) => {
   const { columns, values } = queryResult;
   return (
-    <PieChart width={600} height={400}>
-      <Pie
-        data={values[0].map((value, i) => ({
-          name: "xxx" + i,
-          key: i,
-          value: value,
-        }))}
-        dataKey="value"
-        nameKey="name"
-        cx="50%"
-        cy="50%"
-        outerRadius={60}
-        fill="#8884d8"
-        label={({
-          cx,
-          cy,
-          midAngle,
-          innerRadius,
-          outerRadius,
-          value,
-          index,
-        }) => {
-          const radius = innerRadius + (outerRadius - innerRadius) * 1.4;
-          const RADIAN = Math.PI / 180;
+    <>
+      {values.length > 1 && (
+        <Alert
+          message="Warning: Your query result contains more than 1 row. The chart is displayed based on the first row."
+          type="warning"
+          showIcon
+          closable
+        />
+      )}
+      <PieChart width={600} height={400}>
+        <Pie
+          data={values[0].map((value, i) => ({
+            name: "xxx" + i,
+            key: i,
+            value: value,
+          }))}
+          dataKey="value"
+          nameKey="name"
+          cx="50%"
+          cy="50%"
+          outerRadius={60}
+          fill="#8884d8"
+          label={({
+            cx,
+            cy,
+            midAngle,
+            innerRadius,
+            outerRadius,
+            value,
+            index,
+          }) => {
+            const radius = innerRadius + (outerRadius - innerRadius) * 1.4;
+            const RADIAN = Math.PI / 180;
 
-          const x = cx + radius * Math.cos(-midAngle * RADIAN);
-          const y = cy + radius * Math.sin(-midAngle * RADIAN);
+            const x = cx + radius * Math.cos(-midAngle * RADIAN);
+            const y = cy + radius * Math.sin(-midAngle * RADIAN);
 
-          return (
-            <text
-              x={x}
-              y={y}
-              textAnchor={x > cx ? "start" : "end"}
-              dominantBaseline="central"
-            >
-              {columns[index] + ": " + value}
-            </text>
-          );
-        }}
-      >
-        {values.map((_, index) => (
-          <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-        ))}
-      </Pie>
-    </PieChart>
+            return (
+              <text
+                x={x}
+                y={y}
+                textAnchor={x > cx ? "start" : "end"}
+                dominantBaseline="central"
+              >
+                {columns[index] + ": " + value}
+              </text>
+            );
+          }}
+        >
+          {values.map((_, index) => (
+            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+          ))}
+        </Pie>
+      </PieChart>
+    </>
   );
 };
 
