67d6707389d48d438a7bd288d27427b280928480
Benjamin Arbogast <benjamin.arbogast@gmail.com>
2023-06-18 13:48:59 +0200

    PieChartDisplay: Make data orientation configurable

--- src/display/PieChartDisplay.tsx
+++ src/display/PieChartDisplay.tsx
@@ -1,12 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import { Cell, Pie, PieChart } from "recharts";
-import { Alert } from "antd";
+import { Alert, Select } from "antd";
 import { QueryExecResult } from "../Db";
 import { COLORS } from "../constants";
+
+type Orientation = "row" | "column";
 
 type Props = { queryResult: QueryExecResult };
 const PieChartDisplay: React.FC<Props> = ({ queryResult }) => {
   const { columns, values } = queryResult;
+
+  const defaults =
+    columns.includes("value") && columns.includes("label")
+      ? ({
+          orientation: "column",
+          valueColumnIndex: columns.indexOf("value"),
+          labelColumnIndex: columns.indexOf("label"),
+        } as const)
+      : ({
+          orientation: "row",
+          valueColumnIndex: 0,
+          labelColumnIndex: 0,
+        } as const);
+
+  const [dataOrientation, setDataOrientation] = useState<Orientation>(
+    defaults.orientation
+  );
+  const [valueColumnIndex, setValueColumnIndex] = useState<number>(
+    defaults.valueColumnIndex
+  );
+  const [labelColumnIndex, setLabelColumnIndex] = useState<number>(
+    defaults.labelColumnIndex
+  );
+
+  const chartData =
+    dataOrientation === "row"
+      ? values[0].map((value, i) => ({
+          label: `${columns[i]}: ${value}`,
+          key: i,
+          value: value,
+        }))
+      : values.map((row, i) => ({
+          key: i,
+          label: `${row[labelColumnIndex]}: ${row[
+            valueColumnIndex
+          ]?.toLocaleString()}`,
+          value: row[valueColumnIndex],
+        }));
+
+  console.log(chartData);
+
   return (
     <>
       {values.length > 1 && (
@@ -17,28 +60,52 @@ const PieChartDisplay: React.FC<Props> = ({ queryResult }) => {
           closable
         />
       )}
+      Orientation of data:{" "}
+      <Select
+        value={dataOrientation}
+        onChange={setDataOrientation}
+        options={[
+          { value: "column", label: "Values are in one column" },
+          { value: "row", label: "Values are in one row" },
+        ]}
+        style={{ width: 220 }}
+      />
+      <br />
+      {dataOrientation === "column" && (
+        <>
+          Column containing data:
+          <Select
+            value={valueColumnIndex}
+            onChange={setValueColumnIndex}
+            options={columns.map((col, index) => ({
+              value: index,
+              label: col,
+            }))}
+            style={{ width: 220 }}
+          />
+          <br />
+          Column containing labels:
+          <Select
+            value={labelColumnIndex}
+            onChange={setLabelColumnIndex}
+            options={columns.map((col, index) => ({
+              value: index,
+              label: col,
+            }))}
+            style={{ width: 220 }}
+          />
+        </>
+      )}
       <PieChart width={600} height={400}>
         <Pie
-          data={values[0].map((value, i) => ({
-            name: "xxx" + i,
-            key: i,
-            value: value,
-          }))}
+          data={chartData}
           dataKey="value"
           nameKey="name"
           cx="50%"
           cy="50%"
           outerRadius={60}
           fill="#8884d8"
-          label={({
-            cx,
-            cy,
-            midAngle,
-            innerRadius,
-            outerRadius,
-            value,
-            index,
-          }) => {
+          label={({ cx, cy, midAngle, innerRadius, outerRadius, label }) => {
             const radius = innerRadius + (outerRadius - innerRadius) * 1.4;
             const RADIAN = Math.PI / 180;
 
@@ -52,7 +119,7 @@ const PieChartDisplay: React.FC<Props> = ({ queryResult }) => {
                 textAnchor={x > cx ? "start" : "end"}
                 dominantBaseline="central"
               >
-                {columns[index] + ": " + value}
+                {label}
               </text>
             );
           }}
