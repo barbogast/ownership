a943545846a76c2cf2133e8ed1267961ba876655
Benjamin Arbogast <benjamin.arbogast@gmail.com>
2023-06-20 16:24:45 +0200

    Add another default query

--- src/query/queryStore.ts
+++ src/query/queryStore.ts
@@ -5,7 +5,7 @@ import { immer } from "zustand/middleware/immer";
 
 type ChartType = "table" | "barChart" | "pieChart";
 
-type Query = {
+export type Query = {
   id: string;
   label: string;
   databaseFileName: string;
@@ -19,7 +19,7 @@ type QueryState = {
   queries: { [queryId: string]: Query };
 };
 
-const code = `
+const code1 = `
 return queryResult[0].values.map((row) => {
   const mappedValues = Object.fromEntries(queryResult[0].columns.map((k, i) => [k, row[i]]));
   return {
@@ -47,6 +47,29 @@ return queryResult[0].values.map((row) => {
   };
 });`;
 
+const query = `
+select category_1 from aaa where category_1 is not null group by category_1 order by category_1;
+select category_2 from aaa where category_2 is not null group by category_2 order by category_2;
+select category_3 from aaa where category_3 is not null group by category_3 order by category_3;
+select category_4 from aaa where category_4 is not null group by category_4  order by category_4;`;
+
+const code2 = `
+const valueArrays = queryResult.map(({values}) => values)
+const [cat1, cat2, cat3, cat4] = queryResult.map(res => res.values.map(row => row[0]))
+
+
+const maxLength = Math.max(...valueArrays.map(arr => arr.length))
+
+
+const data = Array(maxLength).fill().map((_, i) => ({
+  category1: cat1[i],
+  category2: cat2[i],
+  category3: cat3[i],
+  category4: cat4[i]
+}))
+return data
+`;
+
 const initialState: QueryState = {
   queries: {
     tableofownershipdetails: {
@@ -55,7 +78,7 @@ const initialState: QueryState = {
       databaseFileName: "database.sqlite",
       sqlStatement: "select * from aaa",
       enableTransform: true,
-      transformCode: code,
+      transformCode: code1,
       chartType: "table",
     },
     tableofownershipdistribution: {
@@ -77,6 +100,15 @@ const initialState: QueryState = {
       enableTransform: false,
       transformCode: "",
       chartType: "barChart",
+    },
+    categoryanalysis: {
+      id: "categoryanalysis",
+      label: "Category Analysis",
+      databaseFileName: "database2.sqlite",
+      sqlStatement: query,
+      enableTransform: true,
+      transformCode: code2,
+      chartType: "table",
     },
   },
 };
