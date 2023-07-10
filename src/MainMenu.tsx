e603603527385344297d121bb9eb7ad2d0a73616
Benjamin Arbogast <benjamin.arbogast@gmail.com>
2023-07-10 12:16:35 +0200

    Improve layout of repository area

--- src/MainMenu.tsx
+++ src/MainMenu.tsx
@@ -1,4 +1,4 @@
-import { Badge, Button, Divider, Menu, Select } from "antd";
+import { Badge, Button, Col, Divider, Menu, Row, Select } from "antd";
 import { ReactElement, useEffect, useState } from "react";
 import { Link, useLocation } from "wouter";
 import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
@@ -143,35 +143,46 @@ const MainMenu: React.FC<Props> = ({ children }) => {
 
   return (
     <PanelGroup direction="horizontal">
-      <Panel defaultSize={20} minSize={10}>
-        <Button onClick={() => setLocation("/")} type="text">
-          ←
-        </Button>
-        <Select
-          options={Object.values(projects).map((project) => ({
-            title: `${project.organization}/${project.repository}`,
-            value: `${project.organization}/${project.repository}`,
-          }))}
-          value={repositoryInfo.path}
-          style={{ width: 200 }}
-          onSelect={(value) => setLocation("/" + value)}
-        />
-        <Button
-          loading={isSaving}
-          onClick={async () => {
-            setIsSaving(true);
-            await saveToGit(repositoryInfo.path);
-            useModifiedStore.setState(() => ({ modifiedQueries: [] }));
-            setIsSaving(false);
-          }}
-        >
-          Save
-        </Button>
-        <Button
-          onClick={() => loadFromGit(repositoryInfo.path).catch(console.error)}
-        >
-          Load
-        </Button>
+      <Panel defaultSize={20} minSize={10} style={{ marginRight: 10 }}>
+        <Row wrap={false}>
+          <Col>
+            <Button onClick={() => setLocation("/")} type="text">
+              ←
+            </Button>
+          </Col>
+          <Col flex={1}>
+            <Select
+              options={Object.values(projects).map((project) => ({
+                title: `${project.organization}/${project.repository}`,
+                value: `${project.organization}/${project.repository}`,
+              }))}
+              value={repositoryInfo.path}
+              style={{ width: "100%" }}
+              onSelect={(value) => setLocation("/" + value)}
+            />
+          </Col>
+        </Row>
+
+        <Row justify="space-between" style={{ marginTop: 10 }}>
+          <Button
+            loading={isSaving}
+            onClick={async () => {
+              setIsSaving(true);
+              await saveToGit(repositoryInfo.path);
+              useModifiedStore.setState(() => ({ modifiedQueries: [] }));
+              setIsSaving(false);
+            }}
+          >
+            Save
+          </Button>
+          <Button
+            onClick={() =>
+              loadFromGit(repositoryInfo.path).catch(console.error)
+            }
+          >
+            Load
+          </Button>
+        </Row>
         <Divider style={{ margin: "12px 0" }} />
         <Menu
           mode="inline"
