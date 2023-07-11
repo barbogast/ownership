1c0338dd56d5aa13aa6c29167d8c57907b0b423b
Benjamin Arbogast <benjamin.arbogast@gmail.com>
2023-07-11 15:42:14 +0200

    Fix log message

--- src/RepositoryStores.tsx
+++ src/RepositoryStores.tsx
@@ -2,7 +2,9 @@ import { ReactElement, useEffect } from "react";
 import { enable } from "./query/queryStore";
 import useReportStore from "./report/reportStore";
 import { useRepoInfo } from "./util/utils";
+import Logger from "./util/logger";
 
+const logger = new Logger("database");
 type Props = {
   children: ReactElement[] | ReactElement;
 };
@@ -10,7 +12,7 @@ const RepositoryStores: React.FC<Props> = ({ children }) => {
   const info = useRepoInfo();
   useEffect(() => {
     if (info) {
-      console.log("hydrate", info);
+      logger.log("hydrate", info);
 
       enable(info);
 
