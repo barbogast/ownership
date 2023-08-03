7ede75b6ef41ddf8e5cb7d2de196d68dfbfe1146
Benjamin Arbogast <benjamin.arbogast@gmail.com>
2023-08-03 11:22:38 +0200

    Disable manual code splitting and the sourceMap exclude hack

--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,16 @@
 import { resolve } from "path";
-import { defineConfig, Plugin } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-
-// Plugin is used to prevent a "JavaScript heap out of memory" error during build
-// by excluding node_modules from sourcemap generation.
-// See https://github.com/vitejs/vite/issues/2433 for details.
-export function sourcemapExclude(): Plugin {
-  return {
-    name: "sourcemap-exclude",
-    transform(code: string, id: string) {
-      if (id.includes("node_modules")) {
-        return {
-          code,
-          // https://github.com/rollup/rollup/blob/master/docs/plugin-development/index.md#source-code-transformations
-          map: { mappings: "" },
-        };
-      }
-    },
-  };
-}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), sourcemapExclude()],
+  plugins: [react()],
   build: {
     sourcemap: true,
     rollupOptions: {
-      output: {
-        manualChunks: (id) => {
-          if (id.includes("node_modules")) {
-            return "vendor";
-          }
-        },
+      input: {
+        main: resolve(__dirname, "index.html"),
+        nested: resolve(__dirname, "nested/index.html"),
       },
     },
   },
