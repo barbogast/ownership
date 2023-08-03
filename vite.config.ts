7b378dcaf782accbcfcee18d199ceaf0b7cec31c
Benjamin Arbogast <benjamin.arbogast@gmail.com>
2023-08-03 11:25:20 +0200

    Remove (accidentally committed) "nested" entry from vite config

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,6 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-        nested: resolve(__dirname, "nested/index.html"),
       },
     },
   },
