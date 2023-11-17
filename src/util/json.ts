8fc0c9a1fdf93afd53e263a7742fb10a75c5539d
Benjamin Arbogast <benjamin.arbogast@gmail.com>
2023-11-17 15:29:18 +0100

    Commit util/json.ts (missing from commit earlier)

---
+++ src/util/json.ts
@@ -0,0 +1,28 @@
+import { ColumnDefinition } from "./database";
+
+export const parseJson = (source: string) => JSON.parse(source);
+
+export const analyseJsonHeader = (data: unknown): ColumnDefinition[] => {
+  const jsonTypeToDbType = (value: unknown) => {
+    switch (typeof value) {
+      case "string":
+        return "text";
+      case "number":
+        return value % 1 === 0 ? "integer" : "real";
+      case "boolean":
+        return "text";
+      default:
+        return "text";
+    }
+  };
+
+  if (!Array.isArray(data)) {
+    return [];
+  }
+
+  return Object.entries(data[0]).map(([key, value]) => ({
+    sourceName: key,
+    dbName: key,
+    type: jsonTypeToDbType(value),
+  }));
+};
