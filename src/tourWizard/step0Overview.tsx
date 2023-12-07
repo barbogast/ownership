a966836bc4dfe9f44b5e82abbcf58d96faad3b5b
Benjamin Arbogast <benjamin.arbogast@gmail.com>
2023-12-07 12:54:57 +0100

    tourWizard: Fix invalid html nesting

--- src/tourWizard/step0Overview.tsx
+++ src/tourWizard/step0Overview.tsx
@@ -16,77 +16,59 @@ const getStep = () => {
           <li>Structured</li>
         </ul>
         <h1>Data Exploration Platform</h1>
-        <p>
-          No lock-in
-          <ul>
-            <li>Export your data at every step</li>
-            <li>
-              ... or access the raw data / configuration in the git repository
-            </li>
-          </ul>
-        </p>
-
-        <p>
-          Client-side
-          <ul>
-            <li>No server required, having a browser is enough</li>
-          </ul>
-        </p>
-        <p>
-          Decentralized
-          <ul>
-            <li>Building on the decentralized nature of git / Github</li>
-            <li>Just fork the repository and continue on your own</li>
-          </ul>
-        </p>
-        <p>
-          Platform agnostic
-          <ul>
-            <li>Executable in browser or on NodeJS</li>
-            <li>Embeddable in other websites</li>
-          </ul>
-        </p>
-        <p>
-          Framework agnostic
-          <ul>
-            <li>
-              Preferring configuration over code (but allowing both) allows you
-              to replace parts or the whole stack
-            </li>
-            <li>
-              while optionally allowing custom code whenever the configuration
-              is not flexible enough
-            </li>
-          </ul>
-        </p>
-        <p>
-          Reproducable
-          <ul>
-            <li>
-              Each step (data generation, selection, transformation and
-              rendering) is available to insepct and verify
-            </li>
-          </ul>
-        </p>
-        <p>
-          Collaborative / Modifiable
-          <ul>
-            <li>
-              Everybody viewer can immediately start to modify the data and
-              charts
-            </li>
-          </ul>
-        </p>
-        <p>
-          Flexible
-          <ul>
-            <li>Enter your data as CSV, or generate it by runnging a script</li>
-            <li>Select your data with SQL or by pivoting</li>
-            <li>Optionally transform the data with code</li>
-            <li>Display as various chart types</li>
-          </ul>
-        </p>
-        <p>Open source</p>
+        No lock-in
+        <ul>
+          <li>Export your data at every step</li>
+          <li>
+            ... or access the raw data / configuration in the git repository
+          </li>
+        </ul>
+        Client-side
+        <ul>
+          <li>No server required, having a browser is enough</li>
+        </ul>
+        Decentralized
+        <ul>
+          <li>Building on the decentralized nature of git / Github</li>
+          <li>Just fork the repository and continue on your own</li>
+        </ul>
+        Platform agnostic
+        <ul>
+          <li>Executable in browser or on NodeJS</li>
+          <li>Embeddable in other websites</li>
+        </ul>
+        Framework agnostic
+        <ul>
+          <li>
+            Preferring configuration over code (but allowing both) allows you to
+            replace parts or the whole stack
+          </li>
+          <li>
+            while optionally allowing custom code whenever the configuration is
+            not flexible enough
+          </li>
+        </ul>
+        Reproducable
+        <ul>
+          <li>
+            Each step (data generation, selection, transformation and rendering)
+            is available to insepct and verify
+          </li>
+        </ul>
+        Collaborative / Modifiable
+        <ul>
+          <li>
+            Everybody viewer can immediately start to modify the data and charts
+          </li>
+        </ul>
+        Flexible
+        <ul>
+          <li>Enter your data as CSV, or generate it by runnging a script</li>
+          <li>Select your data with SQL or by pivoting</li>
+          <li>Optionally transform the data with code</li>
+          <li>Display as various chart types</li>
+        </ul>
+        Open source
       </div>
     ),
   };
